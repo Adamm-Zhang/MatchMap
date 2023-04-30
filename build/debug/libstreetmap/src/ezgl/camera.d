31e3bd92939c6771d4967ecbae2de6215bbfd85c
Ridvik Pal <105998663+ridvikpal@users.noreply.github.com>
2023-04-30 11:59:25 -0400

    Add files via upload
    Added partial build files

---
+++ build/debug/libstreetmap/src/ezgl/camera.d
@@ -0,0 +1,6 @@
+build/debug/libstreetmap/src/ezgl/camera.o: \
+ libstreetmap/src/ezgl/camera.cpp libstreetmap/src/ezgl/camera.hpp \
+ libstreetmap/src/ezgl/point.hpp libstreetmap/src/ezgl/rectangle.hpp
+libstreetmap/src/ezgl/camera.hpp:
+libstreetmap/src/ezgl/point.hpp:
+libstreetmap/src/ezgl/rectangle.hpp:
